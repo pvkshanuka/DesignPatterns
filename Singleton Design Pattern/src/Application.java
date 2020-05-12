29370426ff4484342c845e8d2672183d142a3145
pvkshanuka <pvkshanuka@gmail.com>
2020-05-12 20:25:52 +0530

    Singleton Design Pattern Update

--- Singleton Design Pattern/src/Application.java
+++ Singleton Design Pattern/src/Application.java
@@ -3,17 +3,23 @@ public class Application {
     public static void main(String[] args) {
 
 
-        System.out.println(School.getInstance()+" : "+School.getStudents().size());
+//        System.out.println(School.getInstance()+" : "+School.getStudents().size());
 
-        School.addStudent(new Student(1,"Kusal"));
+        School school = School.getInstance();
+        school.students.add(new Student(1,"Kusal"));
 
-        System.out.println(School.getInstance()+" : "+School.getStudents().size());
+        System.out.println(school);
+        System.out.println(school.students);
 
-        School.addStudent(new Student(2,"Shanuka"));
+        School school2 = School.getInstance();
+        school2.students.add(new Student(2,"Shanuka"));
 
-        System.out.println(School.getInstance()+" : "+School.getStudents().size());
+        System.out.println(school2);
+        System.out.println(school2.students);
 
-        School.getStudents().forEach(student -> System.out.println(student.getId()+" : "+student.getName()));
+        school.students.forEach(student -> System.out.println(student.getId()+" : "+student.getName()));
+        System.out.println("----");
+        school2.students.forEach(student -> System.out.println(student.getId()+" : "+student.getName()));
 
     }
 }
--- Singleton Design Pattern/src/School.java
+++ Singleton Design Pattern/src/School.java
@@ -2,7 +2,9 @@ import java.util.ArrayList;
 
 public class School {
 
-    private ArrayList<Student> students;
+//    private ArrayList<Student> students;
+
+    public ArrayList<Student> students;
     public static volatile School school;
 
     private School(){
@@ -16,25 +18,26 @@ public class School {
             synchronized (School.class){
                 if (school == null){
                     school = new School();
+                    school.students = new ArrayList<>();
                 }
             }
         }
         return school;
     }
 
-    public static void addStudent(Student student){
-        getStudents().add(student);
-    }
-
-    public static ArrayList<Student> getStudents(){
-        if (getInstance().students == null){
-            synchronized (School.class){
-                if (getInstance().students == null){
-                    school.students = new ArrayList<>();
-                }
-            }
-        }
-        return getInstance().students;
-    }
+//    public static void addStudent(Student student){
+//        getStudents().add(student);
+//    }
+//
+//    public static ArrayList<Student> getStudents(){
+//        if (getInstance().students == null){
+//            synchronized (School.class){
+//                if (getInstance().students == null){
+//                    school.students = new ArrayList<>();
+//                }
+//            }
+//        }
+//        return getInstance().students;
+//    }
 
 }
